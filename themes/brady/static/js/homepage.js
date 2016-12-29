02d0f5fe26db58f06f17367236fcd2a7e7699e7e
Kayla Hennegin <kayla@bellafaithdesign.com>
2016-12-29 00:49:58 -0600

    add updated homepage text and video action

--- themes/brady/static/js/homepage.js
+++ themes/brady/static/js/homepage.js
@@ -1,3 +1,13 @@
+var hideVideo = function(){
+      document.getElementById('new-year-video').pause();
+      $('.video-wrapper').removeClass('show');
+    },
+    showVideo = function(e){
+      e.preventDefault();
+      $('.video-wrapper').addClass('show');
+      document.getElementById('new-year-video').play();
+    };
+
 $(document).ready(function(){
   $('.bxslider').bxSlider({
     auto: true,
@@ -7,4 +17,7 @@ $(document).ready(function(){
       $('.bxslider-wrapper').css('visibility', 'visible');
     }
   });
+
+  $('#new-year-play-button').on('click', showVideo);
+  $('#close-video-button').on('click', hideVideo);
 });
